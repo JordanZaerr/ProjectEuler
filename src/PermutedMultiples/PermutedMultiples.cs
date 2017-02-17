3418e803d42d5d9c72cfee1574b7545827e22045
JordanZaerr <jordan.zaerr@extendhealth.com>
2017-02-17 08:16:19 -0700

    Solved 52

---
+++ src/PermutedMultiples/PermutedMultiples.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Shared;
+
+namespace PermutedMultiples
+{
+    //142857
+    class PermutedMultiples
+    {
+        static void Main()
+        {
+            //~400ms
+            Timer.Record(FirstAttempt);
+            Console.ReadLine();
+        }
+
+        private static void FirstAttempt()
+        {
+            var number = 1;
+            while (true)
+            {
+                var results = Enumerable.Range(1, 6).Select(x => (number * x).ToString().OrderBy(y => y));
+                var set = results.First();
+                if (results.All(x => x.SequenceEqual(set)))
+                {
+                    Console.WriteLine(number);
+                    return;
+                }
+                number++;
+            }
+        }
+    }
+}
