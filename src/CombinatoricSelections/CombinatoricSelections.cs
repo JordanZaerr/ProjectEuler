19a9e5be9870b668af22f040f6c282aa0484d861
JordanZaerr <jordan.zaerr@extendhealth.com>
2017-02-17 09:33:16 -0700

    Solved 53

---
+++ src/CombinatoricSelections/CombinatoricSelections.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Shared;
+
+namespace CombinatoricSelections
+{
+    //4075
+    class CombinatoricSelections
+    {
+        static void Main()
+        {
+            //~130ms
+            Timer.Record(FirstAttempt);
+            Console.ReadLine();
+        }
+
+        private static void FirstAttempt()
+        {
+            var values = Enumerable.Range(1, 100)
+                .SelectMany(n => Enumerable.Range(1, n)
+                .Select(r => n.FactorialBig() / (r.FactorialBig() * (n - r).FactorialBig())))
+                .Where(x => x > 1000000);
+
+            Console.WriteLine(values.Count());
+        }
+    }
+}
